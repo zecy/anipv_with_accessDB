5f83620a41803521c882fa2c95eabaf208a904b9
zecy <angelszc163@gmail.com>
2014-07-15 23:28:18 +0800

    success connect to the accdb

--- access.php
+++ access.php
@@ -1,25 +1,37 @@
 <?php
 /* creat ADO connent */
 
+//phpinfo();
+
 try {
 
-    $database = "D:\\dropbox\\Personal Anime Database\\padb_be.accdb";
-    $db       = new PDO("Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ = $database; Uid=Admin");
+$conn = odbc_connect('animedb','','');
 
-    $sql = "SELECT 鍔ㄧ敾鍩烘湰淇℃伅.鍘熷悕 FROM 鍔ㄧ敾鍩烘湰淇℃伅";
+//    $database = "D:\\dropbox\\Personal Anime Database\\padb_be.accdb";
+//$db       = new PDO("Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ = $database; Uid=Admin");
 
-    foreach ($db -> query($sql) as $row);
-    {
-        print $row['鍘熷悕'] . '<br />';
-    }
+$sql = "SELECT 动画基本信息.原名 as name, 动画基本信息.简称 as shrotname, 动画基本信息.anime_id as id, 动画基本信息.类型 as genre, 动画基本信息.时间 as oa FROM 动画基本信息 WHERE ((动画基本信息.时间) >= #2014/6/1# and (动画基本信息.时间) <= #2014/8/1#)";
 
-    $db = null;
+$rs = odbc_exec($conn, $sql);
+
+while (odbc_fetch_row($rs)) {
+    $name       = odbc_result($rs, "name");
+    $shortname  = odbc_result($rs, "shrotname");
+    $id         = odbc_result($rs, "id");
+    $genre      = odbc_result($rs, "genre");
+    $oa         = odbc_result($rs, "oa");
+
+    echo $oa . "《" . $name . "》" . $genre . "<br/>";
+}
+odbc_close($conn);
+
+$db = null;
 
 }
 
 catch(PDOException $e)
-    {
-        echo $e->getMessage();
-    }
+{
+    echo $e->getMessage();
+}
 
 ?>
